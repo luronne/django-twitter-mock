e4892568bb28bdd89a8315a8517d40176076692d
luronne <luronnee@outlook.com>
2023-04-08 13:19:04 +0100

    Create userprofile APIs

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,18 +1,22 @@
-from django.contrib.auth.models import User
-from rest_framework import viewsets
-from rest_framework import permissions
-from rest_framework.decorators import action
-from rest_framework.response import Response
 from accounts.api.serializers import (
     UserSerializer,
     LoginSerializer,
     SignupSerializer,
+    UserProfileSerializerForUpdate,
+    UserSerializerWithProfile,
 )
 from django.contrib.auth import (
     login as django_login,
     logout as django_logout,
     authenticate as django_authenticate,
 )
+from accounts.models import UserProfile
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from utils.permissions import IsObjectOwner
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
@@ -20,8 +24,8 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
-    serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = UserSerializerWithProfile
+    permission_classes = (IsAuthenticated,)
 
 
 class AccountViewSet(viewsets.ViewSet):
@@ -86,3 +90,12 @@ class AccountViewSet(viewsets.ViewSet):
             'success': True,
             'user': UserSerializer(user).data
         }, status=201)
+
+
+class UserProfileViewSet(
+    viewsets.GenericViewSet,
+    viewsets.mixins.UpdateModelMixin,
+):
+    queryset = UserProfile
+    permission_classes = (IsAuthenticated, IsObjectOwner)
+    serializer_class = UserProfileSerializerForUpdate
