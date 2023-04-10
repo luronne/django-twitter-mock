57dd2f16377a1de7280a2f9d949aac6ac722d8a1
luronne <luronnee@outlook.com>
2023-04-10 21:28:15 +0100

    Create tweet photos model and admin

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,15 +1,38 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
+from testing.testcases import TestCase
 from tweets.models import Tweet
 from datetime import timedelta
 from utils.time_helpers import utc_now
+from tweets.models import TweetPhoto
+from tweets.constants import TweetPhotoStatus
 
 
 class TweetTests(TestCase):
+    def setUp(self):
+        self.user1 = self.create_user('user1')
+        self.tweet = self.create_tweet(self.user1)
 
     def test_hours_to_now(self):
-        test_user = User.objects.create_user(username='testUser')
-        tweet = Tweet.objects.create(user=test_user, content='Test hours_to_now')
-        tweet.created_at = utc_now() - timedelta(hours=10)
-        tweet.save()
-        self.assertEqual(tweet.hours_to_now, 10)
+        self.tweet.created_at = utc_now() - timedelta(hours=10)
+        self.tweet.save()
+        self.assertEqual(self.tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.user1, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+        # double like will not lead to duplicate
+        self.create_like(self.user1, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        user2 = self.create_user('user2')
+        self.create_like(user2, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
+
+    def test_create_photo(self):
+        photo = TweetPhoto.objects.create(
+            tweet=self.tweet,
+            user=self.user1,
+        )
+        self.assertEqual(photo.user, self.user1)
+        self.assertEqual(photo.status, TweetPhotoStatus.PENDING)
+        self.assertEqual(self.tweet.tweetphoto_set.count(), 1)
