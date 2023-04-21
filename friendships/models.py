b92379f4bf5630680b8a9ae4221cc0e39b3446e8
luronne <luronnee@outlook.com>
2023-04-21 17:25:38 +0100

    Add tweet cache

--- friendships/models.py
+++ friendships/models.py
@@ -1,8 +1,9 @@
 from accounts.services import UserService
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import pre_save, pre_delete
+from django.db.models.signals import post_save, pre_delete
 from friendships.listeners import invalidate_following_cache
+from utils.memcached_helper import MemcachedHelper
 
 
 class Friendship(models.Model):
@@ -35,13 +36,13 @@ class Friendship(models.Model):
 
     @property
     def cached_from_user(self):
-        return UserService.get_user_through_cache(self.from_user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.from_user_id)
 
     @property
     def cached_to_user(self):
-        return UserService.get_user_through_cache(self.to_user_id)
+        return MemcachedHelper.get_object_through_cache(User, self.to_user_id)
 
 
 # hook up with listeners to invalidate cache
 pre_delete.connect(invalidate_following_cache, sender=Friendship)
-pre_save.connect(invalidate_following_cache, sender=Friendship)
+post_save.connect(invalidate_following_cache, sender=Friendship)
